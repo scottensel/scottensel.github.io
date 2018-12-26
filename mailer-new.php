f0b840dd0b07fe01e2d2f0cf704cb41e77b54065
Scott Ensel <ScottEnsel@Scotts-MBP.home>
2018-12-26 13:16:19 -0500

    Overwrite

--- mailer-new.php
+++ mailer-new.php
@@ -8,12 +8,12 @@
 
     // Check the data.
     if (empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        header("Location: http://www.scottensel.com/index.php?success=-1#form");
+        header("Location: http://www.saiabithasrinivas.com/index.php?success=-1#form");
         exit;
     }
 
     // Set the recipient email address. Update this to YOUR desired email address.
-    $recipient = "sensel@umich.edu";
+    $recipient = "abithas@umich.edu";
 
     // Set the email subject.
     $subject = "New contact from $name";
